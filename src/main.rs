c9c5162ee0bcd7c4f2ce9ffe0e39394da0be1719
Caitlin Gao <caitlin.gaoo@gmail.com>
2021-01-05 16:07:16 +0800

    optimize(task_demo): optimize command with structopt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::{env, fs};
 use std::io::{stdin, stdout, Write};
 
+use structopt::StructOpt;
+
 use databases::{task, user};
 use constants::TMP_DIR;
 
@@ -8,88 +10,92 @@ mod constants;
 mod databases;
 mod models;
 
-
 fn main() {
-    let args: Vec<String> = env::args().skip(1).collect();
-    if args.len() < 2 {
-        println!("error command");
+    let conf = ApplicationArguments::from_args();
+    if let Some(command) = conf.command {
+        match command {
+            Command::Login(arg) => {
+                handle_user_login(arg);
+            }
+            Command::Logout(_) => {
+                user::logout();
+            }
+            Command::Task(arg) => {
+                handle_task_action(arg);
+            }
+        }
+    }
+}
+
+fn handle_user_login(arg: Login) {
+    println!("Password:");
+    stdout().flush().unwrap();
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    let password = input.trim();
+
+    user::login(&arg.user, password);
+}
+
+fn handle_task_action(arg: Task) {
+    if arg.add.is_some() {
+        task::add_task(&arg.add.unwrap());
         return;
     }
-
-    // 预先创建 tmp 目录，以便于存储用户登录 cache 数据与 task 数据；
-    if fs::metadata(TMP_DIR).is_err() {
-        fs::create_dir(TMP_DIR);
+    if arg.done.is_some() {
+        task::finish_task(arg.done.unwrap());
+        return;
     }
-
-    let cmd = &args[1];
-
-    match &cmd[..] {
-        "login" => {
-            if args.get(2).is_none() {
-                println!("subcommand can not empty.");
-                return;
-            }
-
-            if args[2] != "-u" {
-                println!("Please use -u");
-                return;
-            }
-
-            if args.get(3).is_none() {
-                println!("email can not empty.");
-                return;
-            }
-
-            println!("Password:");
-            stdout().flush().unwrap();
-            let mut input = String::new();
-            stdin().read_line(&mut input).unwrap();
-            let password = input.trim();
-            user::login(&args[3], password);
-        },
-        "logout" => {
-            user::logout();
-        },
-        "add" => {
-            if args.get(2).is_none() {
-                println!("content can not empty.");
-                return;
-            }
-            task::add_task(&args[2]);
-        },
-        "done" => {
-            if args.get(2).is_none() {
-                println!("item id can not empty.");
-                return;
-            }
-            match &args[2].parse::<i32>() {
-                Ok(n) => {
-                    task::finish_task(*n);
-                },
-                Err(_) => {
-                    println!("invalid index, item index must i32");
-                    return;
-                }
-            }
-        },
-        "list" => {
-            if args.get(2).is_none() {
-                task::get_unfinished_tasks();
-            } else {
-                let sub_cmd = &args[2];
-                match &sub_cmd[..] {
-                    "--all" => {
-                        task::get_tasks();
-                    },
-                    _ => {
-                        println!("invalid command, sub command should be --all");
-                        return;
-                    }
-                }
-            }
+    if arg.list {
+        if arg.all {
+            task::get_tasks();
+            return;
         }
-        _ => {
-            println!("invalid command, command should be login | logout | add | done | list");
-        }
+        task::get_unfinished_tasks();
+        return;
     }
+}
+
+#[derive(Debug, StructOpt)]
+pub struct Login {
+    #[structopt(short, long)]
+    pub user: String,
+}
+
+#[derive(Debug, StructOpt)]
+pub struct Logout {
+    #[structopt(short, long)]
+    pub user: Option<String>,
+}
+
+#[derive(Debug, StructOpt)]
+pub struct Task {
+    #[structopt(long)]
+    pub add: Option<String>,
+
+    #[structopt(short, long)]
+    pub done: Option<i32>,
+
+    #[structopt(short, long)]
+    pub list: bool,
+
+    #[structopt(long)]
+    pub all: bool,
+}
+
+#[derive(Debug, StructOpt)]
+pub enum Command {
+    #[structopt(name = "login")]
+    Login(Login),
+    #[structopt(name = "logout")]
+    Logout(Logout),
+    #[structopt(name = "todo")]
+    Task(Task)
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "task")]
+pub struct ApplicationArguments {
+    #[structopt(subcommand)]
+    pub command: Option<Command>,
 }
